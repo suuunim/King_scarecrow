cd4db180e0930c69883a266cc8716650b0e2c43f
Haeun-Y <hahaha329@ewhain.net>
2022-02-10 02:08:52 +0900

    농장 게임 구현

---
+++ Assets/Scripts/farming/RField.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+//전체밭(해당 농작물 밭 6칸 중 아무곳이나) 클릭 
+//관련 스크립트 (raycast target 문제로 작은 밭 한칸 한칸에 적용)
+public class RField : MonoBehaviour
+{
+    //cursorControl
+    //0 : 가장 기본 상태
+    //1 : 무씨앗 누른 상태
+    //2 : 배추 씨앗 누른 상태
+    //3 : 파 씨앗 누른 상태
+    //4 : 물뿌리개 누른 상태
+    //5 : 호미 누른 상태
+
+    //밭 한칸 한칸의 상태
+    //0 기본 상태
+    //1 밭이 갈아진 상태
+    //2 씨앗이 심어진 상태
+    //3 ~ 초 흐름
+
+    public Text cursorControl;
+    public Text result;
+    public Sprite plowed;
+    public Sprite seed;
+    public Sprite complete;
+    public Sprite fail;
+    public Sprite initial;
+
+    public Text seedRNum;
+    
+    private GameObject parent;
+    private Sprite sp;
+
+
+
+    //같은 그룹의 밭들 이미지와 상태 변경하는 함수
+    private void changeImageToWatered()//물줬을 때
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            Transform tmp = parent.transform.GetChild(i);
+            Image img = tmp.GetComponent<Image>();
+           if(tmp.GetChild(0).GetComponent<Text>().text == "2")//이미 씨앗까지 다 준 상태였을 때
+            {
+                Debug.Log(i+ " watered and start growing");
+                tmp.GetChild(0).GetComponent<Text>().text = "3";
+                GameObject.Find("fieldTimer").GetComponent<FieldTimer>().rTimer[i] = 3.0f;
+                Debug.Log(GameObject.Find("fieldTimer").GetComponent<FieldTimer>().rTimer[i]);
+            }
+            
+        }
+        
+    }
+
+    private void changeImageToPlowed()//밭 갈아 줬을 때
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            Transform tmp = parent.transform.GetChild(i);
+            Image img = tmp.GetComponent<Image>();
+            if (tmp.GetChild(0).GetComponent<Text>().text == "0")//기본 상태였을 때
+            {
+                Debug.Log(i+" plowed");
+                Color color = img.color;
+                img.sprite = plowed;
+                color.a = 1.0f;
+                img.color = color;
+                tmp.GetChild(0).GetComponent<Text>().text = "1";
+            }
+
+        }
+
+    }
+    public void clickfield()
+    {
+        Button obj = this.GetComponent<Button>();
+        Image img = obj.image;
+        sp = obj.image.sprite;
+
+        if (obj.transform.GetChild(0).GetComponent<Text>().text == "6")//무 다 자랐을 때
+        {//성공 실패로 나누기
+            if(img.sprite == complete)
+            {
+                Debug.Log("수확성공");
+                result.text = (int.Parse(result.text) + 1).ToString();
+                //json파일도 수정하기
+            }
+            img.sprite = initial;
+            obj.transform.GetChild(0).GetComponent<Text>().text = "0";
+            
+            //수확 성공했으면 수확한 작물 수 ++ 시키기
+        }
+
+
+
+        //무씨앗 클릭 이후 무밭을 클릭하는 경우
+        else if (int.Parse(seedRNum.text) > 0 && cursorControl.text == "1" && obj.transform.parent.name == "field1" && sp == plowed)
+        {
+            
+            Debug.Log(this.GetComponent<Button>() + "seed complete");
+            img.sprite = seed;
+            obj.transform.GetChild(0).GetComponent<Text>().text = "2";
+            //씨앗갯수 감소 코드 추가하기
+            //json 파일 내 변수도 감소시키기
+            seedRNum.text = (int.Parse(seedRNum.text) - 1).ToString();
+        }
+
+        //배추 씨앗 클릭 후 배추밭을 클릭하는 경우
+        else if(cursorControl.text == "2" && this.GetComponent<Button>().name == "field2" && sp == plowed)
+        {
+
+        }
+
+        //파 씨앗 클릭 후 파밭을 클릭하는 경우
+        else if (cursorControl.text == "3" && this.GetComponent<Button>().name == "field3" && sp == plowed)
+        {
+
+        }
+
+        else if (cursorControl.text == "4")
+        {   
+            //밭 새싹 그래픽으로 변경
+            parent = transform.parent.gameObject;
+            changeImageToWatered();
+        }
+       else if(cursorControl.text == "5")
+        {
+            //밭갈아진 그래픽으로 변경
+            parent = transform.parent.gameObject;
+            changeImageToPlowed();
+        }
+        
+      
+        
+
+    }
+}
---
+++ Assets/Scripts/farming/loadField.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadField : MonoBehaviour
+{
+    public Sprite initial;
+    public Sprite plowed;
+    public Sprite seed_r;
+   /* public Sprite seed_c;
+    public Sprite seed_g;*/
+    public Sprite completed_r;
+    /*public Sprite completed_c;
+    public Sprite completed_g;*/
+    //밭 한칸 한칸의 상태를 로드해주는 함수
+    //0 기본 상태
+    //1 밭이 갈아진 상태
+    //2 씨앗이 심어진 상태
+    //3 물을 준 상태
+    //4 ~ 초 흐름
+
+    void loadFieldR(int[] arr)//무밭
+    {
+        GameObject tmp = null;
+        for (int i = 0; i < 6; i++)
+        {
+            tmp = GameObject.Find("txt_rf" + i.ToString());
+            Image img = tmp.transform.parent.GetComponent<Image>();
+            Color color = img.color;
+            switch (arr[i])
+            {
+                case 0:
+                    img.sprite = initial;
+                    break;
+                case 1:
+                    img.sprite = plowed;
+                    break;
+                case 6:
+                    img.sprite = completed_r;
+                    break;
+                default:
+                    img.sprite = seed_r;
+                    break;
+
+            }
+            color.a = 1.0f;
+            img.color = color;
+            tmp.GetComponent<Text>().text = arr[i].ToString();
+
+        }
+
+    }
+   /* void loadFieldC(int[] arr)//배추밭
+    {
+        GameObject tmp = null;
+        for (int i = 0; i < 6; i++)
+        {
+            tmp = GameObject.Find("txt_cf" + i.ToString());
+            Image img = tmp.transform.parent.GetComponent<Image>();
+            switch(arr[i])
+            {
+                case 0:
+                    img.sprite = initial;
+                    break;
+                case 1:
+                    img.sprite = plowed;
+                    break;
+                case 2:
+                    img.sprite = seed_c;
+                    break;
+                case 10:
+                    img.sprite = completed_c;
+                    break;
+                default:
+                    img.sprite = growing_c;
+                    break;
+
+            }
+            tmp.GetComponent<Text>().text = arr[i].ToString();
+            
+        }
+
+    }
+    void loadFieldG(int[] arr)//파밭
+    {
+        GameObject tmp = null;
+        for (int i = 0; i < 6; i++)
+        {
+            tmp = GameObject.Find("txt_gf" + i.ToString());
+            Image img = tmp.transform.parent.GetComponent<Image>();
+            switch (arr[i])
+            {
+                case 0:
+                    img.sprite = initial;
+                    break;
+                case 1:
+                    img.sprite = plowed;
+                    break;
+                case 2:
+                    img.sprite = seed_g;
+                    break;
+                case 13:
+                    img.sprite = completed_g;
+                    break;
+                default:
+                    img.sprite = growing_g;
+                    break;
+
+            }
+            tmp.GetComponent<Text>().text = arr[i].ToString();
+
+        }
+
+    }*/
+    //농사게임씬 시작시 밭 상태를 불러와서 오브젝트에 저장시킴
+    void Start()
+    {
+        //+)어떤씨앗인지 종류를 구분할 수 있는 씨앗배열, 수확한 작물 수, 밭상태 배열 로드하기
+
+        //이부분 제이슨에서 배열 불러오는걸로 바꾸기
+        int[] tmpArr = { 0, 0, 0, 0, 0, 0};
+        loadFieldR(tmpArr);
+
+        //제이슨에서 수확량, 씨앗갯수 불러오기
+    }
+
+}
