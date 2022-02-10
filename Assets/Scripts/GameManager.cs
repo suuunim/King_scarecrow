e14f223a61b278acfb8237c3fc9133e89a167501
suuunim <87135671+suuunim@users.noreply.github.com>
2022-02-10 18:27:32 +0900

    Feat : 맵 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public int placeNow; //0은 맵, 1은 집, 2는 허수아비, 3은 아주머니 이런식
     public int npcNow;
     public int[] task = { 0, 0, 0 };  // index 0은 허수아비, 1은 아주머니, 2는 이장 이런식
-    public string[] npc = {"허수아비", "아주머니"};
-
-    void Awake()
-    {
-        placeNow = 2;
+    public string[] npc = { "허수아비", "아주머니" };
 
     public static int FindRoot;
 
@@ -44,7 +41,6 @@ public class GameManager : MonoBehaviour
 
     }
 
-   
 
 
     void Start()
