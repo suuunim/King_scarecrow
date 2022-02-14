682b7700fb28b45ce59ae88563b52a65b5322dba
Haeun-Y <hahaha329@ewhain.net>
2022-02-15 04:28:01 +0900

    Revert "사용자 입력 및 게임 시작 버튼 수정"
    This reverts commit 5b8f8a05a094a6b9f03e0610acd2bdca4e4feafc.

--- Assets/Scripts/firstScene/NameSetting.cs
+++ Assets/Scripts/firstScene/NameSetting.cs
@@ -6,11 +6,11 @@ using UnityEngine.UI;
 
 public class NameSetting : MonoBehaviour
 {
-    //public InputField _userName;
+    public InputField _userName;
     // Start is called before the first frame update
     public void clickGameStart()
     {
-        //DataController.Instance.gameData.userName = _userName.text;
+        DataController.Instance.gameData.userName = _userName.text;
         if (DataController.Instance.gameData.isFirstTime)//첫 시작일때
         {
             DataController.Instance.gameData.isFirstTime = false;
