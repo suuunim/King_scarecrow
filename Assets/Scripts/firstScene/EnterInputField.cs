f9e0b5b8cfa310cc928b205dde600a39f2beffca
Haeun-Y <hahaha329@ewhain.net>
2022-02-15 04:50:51 +0900

    사용자 이름 입력 수정

--- Assets/Scripts/firstScene/EnterInputField.cs
+++ Assets/Scripts/firstScene/EnterInputField.cs
@@ -6,15 +6,15 @@ using UnityEngine.UI;
 public class EnterInputField : MonoBehaviour
 {
     public InputField inputField_name;
-    private Button btn_game_start;
+    public Button btn_game_start;
 
     bool check()
     {
-        btn_game_start = GameObject.Find("btn_gameStart").GetComponent<Button>();
         if(inputField_name.text.Length > 0 && Input.GetKey(KeyCode.Return))//사용자가 이름을 입력했을 때
         {
             Debug.Log("사용자가 엔터 누름");
             btn_game_start.gameObject.SetActive(true);
+            inputField_name.gameObject.SetActive(false);
 
         }
         return true;
@@ -22,7 +22,7 @@ public class EnterInputField : MonoBehaviour
     // Start is called before the first frame update
     void Update()
     {
-        
+        check();
     }
 
 }
