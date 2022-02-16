067419aa7a277dea99011d8917e04c67f0d3c37c
suuunim <87135671+suuunim@users.noreply.github.com>
2022-02-17 03:04:32 +0900

    Feat : 이미지 수정 및 사운드 조정
    주인공 이미지 수정 및 사운드 일부 수정

--- Assets/Scripts/Part1/Part1_Load.cs
+++ Assets/Scripts/Part1/Part1_Load.cs
@@ -19,17 +19,28 @@ public class Part1_Load : MonoBehaviour
 
     GameObject npc;
     public GameManager manager;
-
+    public AudioClip engine;
+    public AudioSource audioSource;
 
     public GameObject farmer_F;
-  
+
 
     public GameObject backgroud_home;
     public GameObject backgroud_company;
 
     string[] script_list_1 = new string[] { "이젠 휴대폰 배터리마저 없네. 이제 진짜 힘든데..", "어디 지나가는 사람 한 명만 나타났으면 좋겠다. 아무나라도 제발 좀..", "청년!", " 어… 와, 사람이다.", "여기서 뭐하고 있어? 상태가 많이 안 좋아보이는데, 내가 좀 도와줘?", "네.. 네!!!!! 감사합니다. 정말 감사해요..", "차에 좀 타도 될까요? 제가 지금 너무 힘들어서요.", "그럼! 얼른 타." };
-    string[] script_list_2 = new string[] {"이건 꿈일 거야. 집에 가서 잤다가 깨면, 나는… 다른 곳에 있을 거야. 확실해." };
+    string[] script_list_2 = new string[] { "이건 꿈일 거야. 집에 가서 잤다가 깨면, 나는… 다른 곳에 있을 거야. 확실해." };
     string[] script_list = new string[] { };
+
+    void Awake()
+    {
+
+
+
+
+        this.audioSource = GetComponent<AudioSource>();
+
+    }
 
     public void OnClickNextText()
     {
@@ -57,12 +68,14 @@ public class Part1_Load : MonoBehaviour
             else if (clickCount == 2)
             {
                 farmer_F.SetActive(true);
+                playSound("engine");
             }
 
 
 
         }
-        else if (GameManager.Part1 == 17) {
+        else if (GameManager.Part1 == 17)
+        {
 
             if (clickCount == 1)
             {
@@ -75,7 +88,7 @@ public class Part1_Load : MonoBehaviour
         }
 
 
-            string str = script_list[clickCount];
+        string str = script_list[clickCount];
 
 
 
@@ -83,14 +96,29 @@ public class Part1_Load : MonoBehaviour
         talk.SetMsg(str);
 
         clickCount++;
-        
+
 
 
 
     }
 
 
+    void playSound(String action)
+    {
 
+        switch (action)
+        {
+            case "engine":
+                audioSource.clip = engine;
+                break;
+
+
+
+        }
+
+        audioSource.Play();
+
+    }
     public void StartTalk()
     {
 
@@ -108,7 +136,7 @@ public class Part1_Load : MonoBehaviour
         talkUI.SetActive(true);
         talkUI.transform.GetChild(1).gameObject.SetActive(true);
 
-        if (GameManager.Part1 ==16)
+        if (GameManager.Part1 == 16)
         {
             talk.SetMsg("지금이 몇 시지. 도대체 얼마나 걸은 걸까. 목도 마르고, 다리도 아프고..");
             for (int i = 0; i < script_list_1.Length; i++)
@@ -119,7 +147,8 @@ public class Part1_Load : MonoBehaviour
             }
 
         }
-        else if (GameManager.Part1 == 17) {
+        else if (GameManager.Part1 == 17)
+        {
             farmer_F.SetActive(false);
             talk.SetMsg("허… 정신 나간 마을 주민이 한 명 더 있었어! 돌고 돌아 또 이 마을이라니.");
             for (int i = 0; i < script_list_2.Length; i++)
